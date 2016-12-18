4af1bb896461460d70ce5dff9aa8269b54ab8ba8
Jake Day Williams <jake@wildacademy.com>
2016-12-17 21:06:19 -0700

    Switch haml creation of post model controller and views

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :url, :body)
+  end
+
+end
