41aef8be09af877c24a62f3ab71a1c9daf67571d
Jake Day Williams <jake@wildacademy.com>
2016-12-18 01:53:20 -0700

    Styling to post index and view pages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -23,6 +24,10 @@ class PostsController < ApplicationController
 
   private
 
+  def find_post
+    @post = Post.find(params[:id])
+  end
+  
   def post_params
     params.require(:post).permit(:title, :url, :body)
   end
