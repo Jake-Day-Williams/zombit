3fd8c2fe28c2bfea8022713ede3c213f22b5d1a7
Jake Day Williams <jake@wildacademy.com>
2017-01-05 17:18:23 -0700

    votable routes and glyphs

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :posts
-  root "posts#index"  
+
+  resources :posts do
+    member do
+      put "like",    to: "posts#upvote"
+      put "dislike", to: "posts#downvote"
+    end
+  end
+
+  root "posts#index"
 end
