daeb3315000327e382439d29f354df8eaacd5e9e
Jake Day Williams <jake@wildacademy.com>
2017-01-10 00:22:18 -0700

    Update signup path

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
 
   def index
     sorted_posts = Post.all.order('cached_votes_score desc')
-    @posts = sorted_posts.paginate(:page => params[:page], :per_page => 3)
+    @posts = sorted_posts.paginate(:page => params[:page], :per_page => 5)
   end
 
   def show
